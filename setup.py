01d5dd6d5c8ef82df4da35ec9125204c6baa8b17
Dana Simard <dana.simard@astro.caltech.edu>
2019-10-25 12:16:08 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dsa110-calib',
+      version='0.1',
+      url='http://github.com/dsa110/dsa110-calib/',
+      author='Dana Simard',
+      author_email='dana.simard@astro.caltech.edu',
+      packages=['dsacalib'],
+      zip_safe=False)
+      
