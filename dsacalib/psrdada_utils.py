3b67afb85d5f09b30c58edefc799259c1b148be3
dsa <dsa@github.com>
2020-01-31 18:29:24 +0000

    sped up to 6144 channels

--- dsacalib/psrdada_utils.py
+++ dsacalib/psrdada_utils.py
@@ -11,10 +11,20 @@ def read_header(reader):
 def read_buffer(reader, nbls, nchan, npol):
     """
     Reads a psrdada buffer as unsigned shorts and returns the visibilities.
+    
+    Args:
+      reader: psrdada Reader instance
+        an instance of the Reader class for the psrdada buffer you want to read
+      nbls: int
+        the number of baselines
+      nchan: int
+        the number of frequency channels
+      npol: int 
+        the number of polarizations
 
-    nint is the number of integrations you will be using after fringestopping.
-    This is used in the event of an incomplete frame to make sure the output 
-    data will have an appropriate shape.
+    Returns:
+      data: ndarray
+        dimensions time, baselines, channels, polarization
     """
     page = reader.getNextPage()
     reader.markCleared()
@@ -26,10 +36,9 @@ def read_buffer(reader, nbls, nchan, npol):
         print('incomplete data: {0} out of {1} samples'.format(data.shape[0]%(nbls*nchan*npol),
                                                                nbls*nchan*npol))
         data = data[:data.shape[0]//(nbls*nchan*npol)*(nbls*nchan*npol)].reshape(-1,nbls,nchan,npol)
-        #if data.shape[0]%nint !=0:
-        #    data = data[:data.shape[0]//nint*nint,...]
-    data = data.swapaxes(0,1)
-    data = data[::-1,...]
+    # Remove reshaping needed for casa order
+    #data = data.swapaxes(0,1)
+    #data = data[::-1,...]
     return data
 
 def update_time(tstart,samples_per_frame,sample_rate):
@@ -37,7 +46,7 @@ def update_time(tstart,samples_per_frame,sample_rate):
     tstart += samples_per_frame/sample_rate
     return t,tstart
 
-def get_antpos(antenna_order,antpos):
+def get_antpos(antenna_order,antpos,casaorder=True):
     aname = antenna_order[::-1]
     tp    = np.loadtxt(antpos)
     blen  = []
@@ -49,8 +58,9 @@ def get_antpos(antenna_order,antpos):
             bname.append([a1+1,a2+1])
             blen.append(tp[a1,1:]-tp[a2,1:])
     blen  = np.array(blen)
-    blen = blen[::-1]
-    bname = bname[::-1]
+    if casaorder:
+        blen = blen[::-1]
+        bname = bname[::-1]
     return blen, bname
 
 def integrate(data,nint):
