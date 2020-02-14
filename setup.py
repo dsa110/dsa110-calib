ff63847b6868a15df5d201084b5b54d26ba3c8aa
dsa <dsa@github.com>
2020-02-14 22:21:05 +0000

    example notebook

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dsa110-calib',
-      version='0.2',
+      version='0.3',
       url='http://github.com/dsa110/dsa110-calib/',
       author='Dana Simard',
       author_email='dana.simard@astro.caltech.edu',
