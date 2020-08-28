693b2d727c7d92f5c43f751c883677f405e992fc
Dana Simard <dana.simard@astro.caltech.edu>
2020-08-28 15:58:15 -0700

    iers in test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
 import pytest
 
 import astropy.units as u
+from astropy.utils import iers
+iers.conf.iers_auto_url_mirror = ct.IERS_TABLE
+iers.conf.auto_max_age = None
 from astropy.time import Time
 from dsacalib import utils, constants
 import numpy as np
