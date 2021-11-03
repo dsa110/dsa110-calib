eab8e1dafaaeb4e6d92141e1e03ba6efa12be85d
Dana Simard <dana.simard@astro.caltech.edu>
2021-11-03 11:04:18 -0700

    first attempt at refactoring bfweights stuff

--- dsacalib/weights.py
+++ dsacalib/weights.py
@@ -1,9 +1,15 @@
 import glob
 import os
+import yaml
 import numpy as np
 import matplotlib.pyplot as plt
-from astropy import time, coordinates, units
+import astropy.units as u
+from antpos.utils import get_itrf
+from casacore.tables import table
 from dsautils import cnf
+import dsacalib.constants as ct
+from dsacalib.fringestopping import calc_uvw
+from dsacalib.ms_io import read_caltable, get_antenna_gains, get_delays
 
 CONF = cnf.Conf()
 
@@ -23,8 +29,10 @@ def get_bfnames(select=None):
     """ Run on dsa-storage to get file names for beamformer weight files.
     Returns list of calibrator-times that are used in data file name.
     """
-
-    fn_dat = glob.glob(os.path.join(BEAMFORMER_DIR, 'beamformer_weights*corr07*.dat'))
+    # TODO: Use refcorr
+    fn_dat = glob.glob(
+        os.path.join(BEAMFORMER_DIR, 'beamformer_weights*corr07*.dat')
+    )
 
     bfnames = []
     for fn in fn_dat:
@@ -32,7 +40,7 @@ def get_bfnames(select=None):
         if len(sp) > 1:
             if '_' in sp[1]:
                 bfnames.append(sp[1].rstrip('.dat'))
-    bfnames = sorted(bfnames)
+    bfnames = sorted(bfnames, reverse=True)
 
     # select subset
     if select is not None:
@@ -48,16 +56,19 @@ def read_gains(bfnames):
     """ Reads gain for each of the data files in bfnames.
     Returns gain array with same length as bfnames.
     """
-
+    # TODO: Needs to not throw an error if a corr doesn't exist
     gains = np.zeros((len(bfnames), len(ANTENNAS), len(CORR_LIST), 48, 2),
                      dtype=np.complex)
 
     for i, beamformer_name in enumerate(bfnames):
         for corridx, corr in enumerate(CORR_LIST):
-            with open('{0}/beamformer_weights_corr{1:02d}_{2}.dat'.format(BEAMFORMER_DIR,
-                                                                          corr,
-                                                                          beamformer_name),
-                      'rb') as f:
+            with open(
+                '{0}/beamformer_weights_corr{1:02d}_{2}.dat'.format(
+                    BEAMFORMER_DIR,
+                    corr,
+                    beamformer_name),
+                'rb'
+            ) as f:
                 data = np.fromfile(f, '<f4')
             temp = data[64:].reshape(64, 48, 2, 2)
             gains[i, :, corridx, :, :] = temp[..., 0]+1.0j*temp[..., 1]
@@ -99,14 +110,14 @@ def find_good_solutions(bfnames, gains, threshold=60, plot=False):
         plt.xlabel('antenna')
         plt.ylabel('calibrator')
         plt.show()
-        
+
     return keep
 
 
 def show_gains(bfnames, gains, keep):
     """ Given bfnames and gains, plot the good gains.
     """
-    
+
     refant = 24
     nx = 4
     ny = len(ANTENNAS)//nx
@@ -121,9 +132,11 @@ def show_gains(bfnames, gains, keep):
     ax = ax.flatten()
 
     for i in np.arange(len(ANTENNAS)):
-        ax[i].imshow(np.angle(gains[:, i, :, 0]/gains[:, 0, :, 0]).take(keep, axis=0),
-                     vmin=-np.pi, vmax=np.pi, aspect='auto', origin='lower', interpolation='None',
-                     cmap=plt.get_cmap('RdBu'))
+        ax[i].imshow(
+            np.angle(gains[:, i, :, 0]/gains[:, 0, :, 0]).take(keep, axis=0),
+            vmin=-np.pi, vmax=np.pi, aspect='auto', origin='lower',
+            interpolation='None', cmap=plt.get_cmap('RdBu')
+        )
         ax[i].annotate('{0}'.format(ANTENNAS[i]), (0, 1), xycoords='axes fraction')
         ax[i].set_xlabel('Frequency channel')
     plt.show()
@@ -133,7 +146,8 @@ def get_good_solution(select=None, plot=False):
     """ Find good set of solutions and calculate average gains.
     TODO: go from good bfnames to average gains.
     """
-
+    # TODO: Needs to include the past 24 hours, not just today.
+    # Can include more than the past 24 hours, as this can be filtered later.
     from datetime import date
 
     if select is None:
@@ -141,10 +155,395 @@ def get_good_solution(select=None, plot=False):
         select = f'{today.year}-{today.month}-{today.day:02}'
 
     print(f'Selecting for string {select}')
-    bfnames = weights.get_bfnames(select=select)
-    gains = weights.read_gains(bfnames)
-    good = weights.find_good_solutions(bfnames, gains, plot=plot, threshold=60)
+    bfnames = get_bfnames(select=select)
+    gains = read_gains(bfnames)
+    good = find_good_solutions(bfnames, gains, plot=plot, threshold=60)
     if plot:
-        weights.show_gains(bfnames, gains, good)
+        show_gains(bfnames, gains, good)
 
     return bfnames[good]
+
+def filter_beamformer_solutions(beamformer_names, start_time):
+    """Removes beamformer solutions inconsistent with the latest solution.
+    """
+    if os.path.exists(
+        '{0}/beamformer_weights_{1}.yaml'.format(
+            BEAMFORMER_DIR,
+            beamformer_names[0]
+        )
+    ):
+        with open(
+            '{0}/beamformer_weights_{1}.yaml'.format(
+                BEAMFORMER_DIR,
+                beamformer_names[0]
+            )
+        ) as f:
+            latest_solns = yaml.load(f, Loader=yaml.FullLoader)
+        for bfname in beamformer_names[1:].copy():
+            try:
+                with open(
+                    '{0}/beamformer_weights_{1}.yaml'.format(
+                        BEAMFORMER_DIR,
+                        bfname
+                    )
+                ) as f:
+                    solns = yaml.load(f, Loader=yaml.FullLoader)
+                assert solns['cal_solutions']['antenna_order'] == \
+                    latest_solns['cal_solutions']['antenna_order']
+                assert solns['cal_solutions']['corr_order'] == \
+                    latest_solns['cal_solutions']['corr_order']
+                assert solns['cal_solutions']['delays'] == \
+                    latest_solns['cal_solutions']['delays']
+                assert solns['cal_solutions']['eastings'] == \
+                    latest_solns['cal_solutions']['eastings']
+                assert solns['cal_solutions']['caltime'] > \
+                    latest_solns['cal_solutions']['caltime']-1
+                assert solns['cal_solutions']['caltime'] > start_time
+            except (AssertionError, FileNotFoundError):
+                beamformer_names.remove(bfname)
+    else:
+        beamformer_names = []
+        latest_solns = None
+    return beamformer_names, latest_solns
+
+def average_beamformer_solutions(
+    fnames, ttime, corridxs=None, tol=0.3
+):
+    """Averages written beamformer solutions.
+
+    Parameters
+    ----------
+    fnames : list
+    ttime : astropy.time.Time object
+        A time to use in the filename of the solutions, indicating when they
+        were written or are useful. E.g. the transit time of the most recent
+        source being averaged over.
+    corridxs : list
+        The correlator nodes for which to average beamformer solutions.
+        Defaults to 1 through 16 inclusive.
+
+    Returns
+    -------
+    written_files : list
+        The names of the written beamformer solutions (one for each correlator
+        node).
+    antenna_flags_badsolns:
+        Flags for antenna/polarization dimensions of gains.
+    """
+    if corridxs is None:
+        corridxs = [
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16
+        ]
+    gains = read_gains(fnames)
+    antenna_flags = [None]*len(fnames)
+    eastings = None
+    for i, fname in enumerate(fnames):
+        tmp_antflags = []
+        filepath = f'{BEAMFORMER_DIR}/beamformer_weights_{fname}.yaml'
+        with open(filepath) as f:
+            calibration_params = yaml.load(
+                f, Loader=yaml.FullLoader
+            )['cal_solutions']
+            antenna_order = calibration_params['antenna_order']
+            for key in calibration_params['flagged_antennas']:
+                if 'casa solutions flagged' in \
+                    calibration_params['flagged_antennas'][key]:
+                    antname = int(key.split(' ')[0])
+                    tmp_antflags.append(antenna_order.index(antname))
+        antenna_flags[i] = sorted(tmp_antflags)
+        gains[i, :, antenna_flags[i], ... ] = np.nan
+
+    j=0
+    while j < len(corridxs) and eastings is None:
+        corr = corridxs[j]
+        filepath = f'{BEAMFORMER_DIR}/beamformer_weights_corr{corr:02d}_{fnames[0]}.dat'
+        if os.path.exists(filepath):
+            with open(filepath) as f:
+                data = np.fromfile(f, '<f4')
+                eastings = data[:64]
+
+    gains = np.nanmean(gains, axis=0)
+    fracflagged = np.sum(np.sum(np.sum(
+        np.isnan(gains),
+        axis=4), axis=2), axis=0)\
+        /(gains.shape[0]*gains.shape[2]*gains.shape[4])
+    antenna_flags_badsolns = fracflagged > tol
+    gains[np.isnan(gains)] = 0.
+    written_files = []
+    if eastings is not None:
+        for i, corr in enumerate(corridxs):
+            fnameout = f'beamformer_weights_corr{corr:02d}_{ttime.isot}'
+            wcorr = gains[i, ...].flatten()
+            wcorr = np.concatenate([eastings, wcorr], axis=0)
+            with open(f'{BEAMFORMER_DIR}/{fnameout}.dat', 'wb') as f:
+                f.write(bytes(wcorr))
+            written_files += ['{0}.dat'.format(fnameout)]
+    return written_files, antenna_flags_badsolns
+
+def write_beamformer_weights(msname, calname, caltime, antennas,
+                             corr_list, antenna_flags, tol=0.3):
+    """Writes weights for the beamformer.
+
+    Parameters
+    ----------
+    msname : str
+        The prefix of the measurement set.  Will open `msname`.ms
+    calname : str
+        The name of the calibrator source.
+    antennas : list
+        The names of the antennas to extract solutions for.  Order must be the
+        same as the order in the beamformer.
+    corr_list : list
+        The indices of the correlator machines to write beamformer weights for.
+        For now, these must be ordered so that the frequencies are contiguous
+        and they are in the same order or the reverse order as in the ms. The
+        bandwidth of each correlator is pulled from dsa110-meridian-fs package
+        data.
+    antenna_flags : ndarray(bool)
+        Dimensions (antennas, pols). True where flagged, False otherwise.
+    tol : float
+        The fraction of data for a single antenna/pol flagged that the
+        can be flagged in the beamformer. If more data than this is flagged as
+        having bad solutions, the entire antenna/pol pair is flagged.
+
+    Returns
+    -------
+    corr_list : list
+    bu : array
+        The length of the baselines in the u direction for each antenna
+        relative to antenna 24.
+    fweights : ndarray
+        The frequencies corresponding to the beamformer weights, dimensions
+        (correlator, frequency).
+    filenames : list
+        The names of the file containing the beamformer weights.
+    """
+    # Get the frequencies we want to write solutions for.
+    # corr_settings = resource_filename("dsamfs", "data/dsa_parameters.yaml")
+    # params = yaml.safe_load(fhand)
+    ncorr = len(corr_list)
+    weights = np.ones((ncorr, len(antennas), 48, 2), dtype=np.complex64)
+    fweights = np.ones((ncorr, 48), dtype=np.float32)
+    nchan = CORR_PARAMS['nchan']
+    dfreq = CORR_PARAMS['bw_GHz']/nchan
+    if CORR_PARAMS['chan_ascending']:
+        fobs = CORR_PARAMS['f0_GHz']+np.arange(nchan)*dfreq
+    else:
+        fobs = CORR_PARAMS['f0_GHz']-np.arange(nchan)*dfreq
+    nchan_spw = CORR_PARAMS['nchan_spw']
+    for i, corr_id in enumerate(corr_list):
+        ch0 = CORR_PARAMS['ch0']['corr{0:02d}'.format(corr_id)]
+        fobs_corr = fobs[ch0:ch0+nchan_spw]
+        fweights[i, :] = fobs_corr.reshape(
+            fweights.shape[1],
+            -1
+        ).mean(axis=1)
+
+    # TODO: move to eastings function
+    # TODO: Use reftime to calc uvw coords
+    antpos_df = get_itrf(
+        latlon_center=(ct.OVRO_LAT*u.rad, ct.OVRO_LON*u.rad, ct.OVRO_ALT*u.m)
+    )
+    blen = np.zeros((len(antennas), 3))
+    for i, ant in enumerate(antennas):
+        blen[i, 0] = antpos_df['x_m'].loc[ant]-antpos_df['x_m'].loc[24]
+        blen[i, 1] = antpos_df['y_m'].loc[ant]-antpos_df['y_m'].loc[24]
+        blen[i, 2] = antpos_df['z_m'].loc[ant]-antpos_df['z_m'].loc[24]
+    bu, _, _ = calc_uvw(blen, 59000., 'HADEC', 0.*u.rad, 0.6*u.rad)
+    bu = bu.squeeze().astype(np.float32)
+
+    with table('{0}.ms/SPECTRAL_WINDOW'.format(msname)) as tb:
+        fobs = np.array(tb.CHAN_FREQ[:])/1e9
+    fobs = fobs.reshape(fweights.size, -1).mean(axis=1)
+    f_reversed = not np.all(
+        np.abs(fobs-fweights.ravel())/fweights.ravel() < 1e-5
+    )
+    if f_reversed:
+        assert np.all(
+            np.abs(fobs[::-1]-fweights.ravel())/fweights.ravel() < 1e-5
+        )
+
+    gains, _time, flags, ant1, ant2 = read_caltable(
+        '{0}_{1}_gacal'.format(msname, calname), True)
+    gains[flags] = np.nan
+    gains = np.nanmean(gains, axis=1)
+    phases, _, flags, ant1p, ant2p = read_caltable(
+        '{0}_{1}_gpcal'.format(msname, calname), True)
+    phases[flags] = np.nan
+    phases = np.nanmean(phases, axis=1)
+    assert np.all(ant1p == ant1)
+    assert np.all(ant2p == ant2)
+    gantenna, gains = get_antenna_gains(gains*phases, ant1, ant2)
+
+    bgains, _, flags, ant1, ant2 = read_caltable(
+        '{0}_{1}_bcal'.format(msname, calname), True)
+    bgains[flags] = np.nan
+    bgains = np.nanmean(bgains, axis=1)
+    bantenna, bgains = get_antenna_gains(bgains, ant1, ant2)
+    assert np.all(bantenna == gantenna)
+
+    nantenna = gains.shape[0]
+    npol = gains.shape[-1]
+
+    gains = gains*bgains
+    print(gains.shape)
+    gains = gains.reshape(nantenna, -1, npol)
+    if f_reversed:
+        gains = gains[:, ::-1, :]
+    gains = gains.reshape(nantenna, ncorr, -1, npol)
+    nfint = gains.shape[2]//weights.shape[2]
+    assert gains.shape[2]%weights.shape[2]==0
+
+    gains = np.nanmean(
+        gains.reshape(
+            gains.shape[0], gains.shape[1], -1, nfint, gains.shape[3]
+        ), axis=3
+    )
+    if not np.all(ant2==ant2[0]):
+        idxs = np.where(ant1==ant2)
+        gains = gains[idxs]
+        ant1 = ant1[idxs]
+    for i, antid in enumerate(ant1):
+        if antid+1 in antennas:
+            idx = np.where(antennas==antid+1)[0][0]
+            weights[:, idx, ...] = gains[i, ...]
+
+    fracflagged = np.sum(np.sum(np.isnan(weights), axis=2), axis=0)\
+        /(weights.shape[0]*weights.shape[2])
+    antenna_flags_badsolns = fracflagged > tol
+    weights[np.isnan(weights)] = 0.
+
+    # Divide by the first non-flagged antenna
+    idx0, idx1 = np.nonzero(
+        np.logical_not(
+            antenna_flags + antenna_flags_badsolns
+        )
+    )
+    weights = (
+        weights/weights[:, idx0[0], ..., idx1[0]][:, np.newaxis, :, np.newaxis]
+    )
+    weights[np.isnan(weights)] = 0.
+
+    filenames = []
+    for i, corr_idx in enumerate(corr_list):
+        wcorr = weights[i, ...].view(np.float32).flatten()
+        wcorr = np.concatenate([bu, wcorr], axis=0)
+        fname = 'beamformer_weights_corr{0:02d}'.format(corr_idx)
+        fname = '{0}_{1}_{2}'.format(
+            fname,
+            calname,
+            caltime.isot
+        )
+        if os.path.exists(f'{BEAMFORMER_DIR}/{fname}.dat'):
+            os.unlink(f'{BEAMFORMER_DIR}/{fname}.dat')
+        with open(f'{BEAMFORMER_DIR}/{fname}.dat', 'wb') as f:
+            f.write(bytes(wcorr))
+        filenames += ['{0}.dat'.format(fname)]
+    return corr_list, bu, fweights, filenames, antenna_flags_badsolns
+
+def write_beamformer_solutions(
+    msname, calname, caltime, antennas, applied_delays,
+    corr_list=np.arange(1, 17),
+    flagged_antennas=None,
+    pols=None
+):
+    """Writes beamformer solutions to disk.
+
+    Parameters
+    ----------
+    msname : str
+        The name of the measurement set used for calibration.
+    calname : str
+        The name of the calibrator source used for calibration. Will open
+        tables that start with `msname`_`calname`
+    caltime : astropy.time.Time object
+        The transit time of the calibrator.
+    antennas : list
+        The antenna names for which to write beamformer solutions, in order.
+    applied_delays : ndarray
+        The currently applied delays at the time of the calibration, in ns.
+        Dimensions (antenna, pol). The antenna axis should be in the order
+        specified by antennas.
+    corr_list : list
+        The indices of the correlator machines to write beamformer weights for.
+        For now, these must be ordered so that the frequencies are contiguous
+        and they are in the same order or the reverse order as in the ms. The
+        bandwidth of each correlator is pulled from dsa110-meridian-fs package
+        data.
+    flagged_antennas : list
+        A list of antennas to flag in the beamformer solutions. Should include
+        polarizations. e.g. ['24 B', '32 A']
+    pols : list
+        The order of the polarizations.
+
+    Returns
+    -------
+    flags : ndarray(boolean)
+        Dimensions (antennas, pols). True where the data is flagged, and should
+        not be used. Compiled from the ms flags as well as `flagged_antennas`.
+    """
+    if pols is None:
+        pols = ['B', 'A']
+    beamformer_flags = {}
+    delays, flags = get_delays(antennas, msname, calname, applied_delays)
+    print('delay flags:', flags.shape)
+    if flagged_antennas is not None:
+        for item in flagged_antennas:
+            ant, pol = item.split(' ')
+            flags[antennas==ant, pols==pol] = 1
+            beamformer_flags['{0} {1}'.format(ant, pol)] = ['flagged by user']
+    delays = delays-np.min(delays[~flags])
+    while not np.all(delays[~flags] < 1024):
+        if np.sum(delays[~flags] > 1024) < np.nansum(delays[~flags] < 1024):
+            argflag = np.argmax(delays[~flags])
+        else:
+            argflag = np.argmin(delays[~flags])
+        argflag = np.where(~flags.flatten())[0][argflag]
+        flag_idxs = np.unravel_index(argflag, flags.shape)
+        flags[np.unravel_index(argflag, flags.shape)] = 1
+        key = '{0} {1}'.format(antennas[flag_idxs[0]], pols[flag_idxs[1]])
+        if key not in beamformer_flags.keys():
+            beamformer_flags[key] = []
+        beamformer_flags[key] += ['delay exceeds snap capabilities']
+        delays = delays-np.min(delays[~flags])
+
+    caltime.precision = 0
+    corr_list, eastings, _fobs, weights_files, flags_badsolns = \
+        write_beamformer_weights(msname, calname, caltime, antennas,
+        corr_list, flags)
+    idxant, idxpol = np.nonzero(flags_badsolns)
+    for i, ant in enumerate(idxant):
+        key = '{0} {1}'.format(antennas[ant], pols[idxpol[i]])
+        if key not in beamformer_flags.keys():
+            beamformer_flags[key] = []
+        beamformer_flags[key] += ['casa solutions flagged']
+
+    calibration_dictionary = {
+        'cal_solutions':
+        {
+            'source': calname,
+            'caltime': float(caltime.mjd),
+            'antenna_order': [int(ant) for ant in antennas],
+            'corr_order': [int(corr) for corr in corr_list],
+            'pol_order': ['B', 'A'],
+            'delays': [
+                [
+                    int(delay[0]//2),
+                    int(delay[1]//2)
+                ] for delay in delays
+            ],
+            'eastings': [float(easting) for easting in eastings],
+            'weights_axis0': 'antenna',
+            'weights_axis1': 'frequency',
+            'weights_axis2': 'pol',
+            'weight_files': weights_files,
+            'flagged_antennas': beamformer_flags
+        }
+    }
+
+    with open(
+        f'{BEAMFORMER_DIR}/beamformer_weights_{calname}_{caltime.isot}.yaml',
+        'w'
+    ) as file:
+        yaml.dump(calibration_dictionary, file)
+    return flags
