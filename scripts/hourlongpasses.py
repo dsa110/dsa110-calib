f2979296f80f56ba4a4898136821aeed39dbeb16
Dana Simard <dana.simard@astro.caltech.edu>
2021-12-16 14:58:04 -0800

    script to make long measurement sets

---
+++ scripts/hourlongpasses.py
@@ -0,0 +1,43 @@
+from dsacalib.ms_io import convert_calibrator_pass_to_ms
+from dsacalib.routines import get_files_for_cal
+import astropy.units as u
+from pkg_resources import resource_filename
+
+msdir = '/mnt/data/dsa110/calibration/manual_cal/'
+hdf5dir = '/mnt/data/dsa110/correlator/'
+calpasses = [
+    {
+        'date': '2021-12-14',
+        'name': 'J133108+303032',
+        'dec': '+030p5',
+        'duration': 25*u.min,
+    },
+]
+
+for i, calpass in enumerate(calpasses):
+    print(f'working on {calpass}')
+    date = calpass['date']
+    dec = calpass['dec']
+    duration = calpass['duration']
+    name = calpass['name']
+    calsources = resource_filename(
+        'dsacalib',
+        'data/calibrator_sources_dec{0}.csv'.format(dec)
+    )
+    filenames = get_files_for_cal(
+        calsources,
+        '03',
+        duration,
+        5*u.min,
+        date_specifier='{0}*'.format(date)
+    )
+    cal = filenames[date][name]['cal']
+    files = filenames[date][name]['files']
+    convert_calibrator_pass_to_ms(
+        cal,
+        date,
+        files,
+        duration,
+        msdir=msdir,
+        hdf5dir=hdf5dir
+    )
