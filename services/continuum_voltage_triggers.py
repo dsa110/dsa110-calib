c658e3996b3cc7167f0fde65819c32fb39f1d3ee
Dana Simard <dana.simard@astro.caltech.edu>
2022-04-25 12:28:06 -0700

    Check time to transit within 5 minutes

--- services/continuum_voltage_triggers.py
+++ services/continuum_voltage_triggers.py
@@ -4,6 +4,12 @@ import dsautils.dsa_store as ds
 from dsacalib.preprocess import update_caltable
 
 
+# Aim for ~3 calibrator sources a day
+# One trigger on each source 
+# Stick close to the centre of the primary beam
+
+# TODO: decide on flux scale, check how to put a reasonable trigger name
+
 def continuum_voltage_triggers():
     seconds_per_sidereal_day = 86164.0905*u.s/(360*u.deg)
     etcd = ds.DsaStore()
@@ -25,9 +31,13 @@ def continuum_voltage_triggers():
 
     while True:
         current_pointing = etcd.get_dict('/mon/array/pointing_J2000')
-        time_to_transit = (df['ra']*u.deg - current_pointing['ra_deg']*u.deg)*seconds_per_sidereal_day
-        # Check times and send or queue voltage triggers
+        time_to_transit = (calsources['ra']*u.deg - current_pointing['ra_deg']*u.deg)*seconds_per_sidereal_day
+        # Check times and send or queue voltage triggers dumps without about 10 s
+        if time_to_transit > -2.5*u.min and time_to_transit < 2.5*u.min:
+            etcd.put_dict('/cmd/corr/0', {'cmd': 'ctrltrigger', 'val': calsources['name']})
+
+        time.sleep(5*60)
 
 
 if __name__ == '__main__':
-    continuum_voltage_triggers() 
+    continuum_voltage_triggers()
