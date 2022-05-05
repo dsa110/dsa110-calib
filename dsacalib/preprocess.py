c3c4905c012c5148b5ef74dd012ab0fb191299f2
Dana Simard <dana.simard@astro.caltech.edu>
2022-05-05 16:01:47 -0700

    using vla calibrator list instead of nvss

--- dsacalib/preprocess.py
+++ dsacalib/preprocess.py
@@ -9,6 +9,7 @@ from urllib.request import urlretrieve
 from collections import namedtuple
 
 import astropy.units as u
+from astropy.coordinates import Angle
 from dsautils import cnf
 import dsautils.dsa_syslog as dsl
 import numpy as np
@@ -151,7 +152,10 @@ def fscrunch_file(fname):
 
 
 def read_nvss_catalog():
-    """Reads the NVSS catalog into a pandas dataframe."""
+    """
+    Reads the NVSS catalog into a pandas dataframe.
+    Returns flux in mJy.
+    """
     if not resource_exists("dsacalib", "data/heasarc_nvss.tdat"):
         urlretrieve(
             "https://heasarc.gsfc.nasa.gov/FTP/heasarc/dbase/tdat_files/heasarc_nvss.tdat.gz",
@@ -201,6 +205,7 @@ def read_vla_catalog():
     """Read the VLA calibrator list into a dataframe.
     Kept source, ra, dec, flux_20_cm keys in the NVSS catalog
     so we can easily switch the two of them out.
+    flux is in mJy.
     """
 
     Calibrator = namedtuple("Calibrator", "source ra dec flux_20_cm code_20_cm")
@@ -209,33 +214,35 @@ def read_vla_catalog():
     with open(filename) as file:
         for _ in range(3):
             file.readline()
+        while True:
+            line = file.readline()
+            if not line:
+                break
+
+            source, _, _, ra, dec, *_ = line.split()
+            ra = Angle(ra).to_value(u.deg)
+            dec = Angle(dec).to_value(u.deg)
+            flux_20_cm = None
+            code_20_cm = None
+            for _ in range(4):
+                file.readline()
+
             while True:
                 line = file.readline()
-                if not line:
+                if line.isspace() or not line:
+                    # We've reached the end of an entry
+                    if flux_20_cm not in [None, '?']:
+                        calsources += [Calibrator(source, ra, dec, 1000*float(flux_20_cm), code_20_cm)]
                     break
-                source, _, _, ra, dec, *_ = line.split()
-                ra = Angle(ra).to_value(u.deg)
-                dec = Angle(dec).to_value(u.deg)
-                flux_20_cm = None
-                code_20_cm = None
-                for _ in range(4):
-                    file.readline()
-
-                while True:
-                    line = file.readline()
-                    if line.isspace() or not line:
-                        # We've reached the end of an entry
-                        calsources += [Calibrator(source, ra, dec, flux_20_cm, code_20_cm)]
-                        break
-                    if "20cm " in line:
-                        _, _, code_a, code_b, code_c, code_d, flux_20_cm, *_ = line.split()
-                        code_20_cm = code_a+code_b+code_c+code_d
+                if "20cm " in line:
+                    _, _, code_a, code_b, code_c, code_d, flux_20_cm, *_ = line.split()
+                    code_20_cm = code_a+code_b+code_c+code_d
     df = pandas.DataFrame.from_records(calsources, columns=Calibrator._fields)
     df.set_index('source', inplace=True)
     return df
 
 
-def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*u.Jy, min_percent_flux=0.15):
+def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*u.Jy, min_percent_flux=0.15, codes=["P", "S"]):
     """Generate a table of calibrators at a given declination.
 
     Parameters
@@ -252,11 +259,12 @@ def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*
         The minimum ratio of the calibrator weighted flux to the weighted flux
         in the primary beam for which to include the calibrator.
     """
-    df = read_nvss_catalog()
+
+    df = read_vla_catalog()
     calibrators = df[
         (df["dec"] < (pt_dec + radius).to_value(u.deg))
         & (df["dec"] > (pt_dec - radius).to_value(u.deg))
-        & (df["flux_20_cm"] > 1000)
+        & (df["flux_20_cm"].astype("float") > 1000)
     ]
     # Calculate field flux and weighted flux for each calibrator
     calibrators = calibrators.assign(field_flux=np.zeros(len(calibrators)))
@@ -289,7 +297,8 @@ def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*
                 )
             )
         calibrators.loc[name, "field_flux"] = sum(field["weighted_flux"])
-    print(calibrators.head)
+ 
+    print(calibrators.head())
     # Calculate percent of the field flux that is contained in the
     # main calibrator
     calibrators = calibrators.assign(
@@ -299,10 +308,12 @@ def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*
     calibrators = calibrators[
         (calibrators["weighted_flux"] > min_weighted_flux.to_value(u.Jy))
         & (calibrators["percent_flux"] > min_percent_flux)
+        & [v[2] in codes for v in  calibrators.loc[:, 'code_20_cm']]
+#        & (calibrators["code_20_cm"][2] == code)  # c-config code match
     ]
-    print(calibrators.head)
+    print(calibrators.head())
     # Create the caltable needed by the calibrator service
-    caltable = calibrators[["ra", "dec", "flux_20_cm", "weighted_flux", "percent_flux"]]
+    caltable = calibrators[["ra", "dec", "flux_20_cm", "weighted_flux", "percent_flux", "code_20_cm"]]
     caltable.reset_index(inplace=True)
     caltable.rename(
         columns={
@@ -315,7 +326,7 @@ def generate_caltable(pt_dec, csv_string, radius=2.5*u.deg, min_weighted_flux=1*
     caltable.loc[:, "source"] = [sname.strip("NVSS ") for sname in caltable["source"]]
     caltable.loc[:, "ra"] = caltable["ra"] * u.deg
     caltable.loc[:, "dec"] = caltable["dec"] * u.deg
-    print(caltable.head)
+    print(caltable.head())
     caltable.to_csv(resource_filename("dsacalib", csv_string))
 
 
